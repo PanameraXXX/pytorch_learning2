d59b789a764be8ec8f098bb7bd63d62cda72c165
PanameraXXX <xujian727292879@126.com>
2022-07-07 00:07:21 +0800

    l_r nn完成

--- linearRegression/l_r_nn.py
+++ linearRegression/l_r_nn.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from torch.utils import data
 from d2l import torch as d2l
+from torch import nn
 
 true_w = torch.tensor([2,-3.4])
 true_b = 4.2
@@ -27,7 +28,23 @@ def load_array(data_arrays, batch_size, is_train=True):
 batch_size = 10
 data_iter = load_array((features,labels),batch_size)
 
+# Sequential： the list of layers
+net = nn.Sequential(nn.Linear(2, 1))
+net[0].weight.data.normal_(0, 0.01)
+net[0].bias.data.fill_(0)
+
+loss = nn.MSELoss()
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+
 next(iter(data_iter))
 
 if __name__ == '__main__':
-    pass
+    num_epochs = 3
+    for epoch in range(num_epochs):
+        for X, y in data_iter:
+            l = loss(net(X), y)
+            trainer.zero_grad() # 先把梯度清理
+            l.backward() # 再计算梯度
+            trainer.step()
+        l = loss(net(features),labels)
+        print(f'epoch{epoch+1}, loss {l:f}')
